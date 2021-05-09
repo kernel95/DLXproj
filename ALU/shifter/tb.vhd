baa236a7c008bbc077c885cf6d9621a645885670
nicosilverx <nicosilverx@gmail.com>
2021-05-09 12:35:02 +0200

    shifter: added enable signal

--- ALU/shifter/tb.vhd
+++ ALU/shifter/tb.vhd
@@ -7,25 +7,32 @@ end tb;
 
 architecture tb of tb is
 
-component t2_shifter is
+component shifter is
     generic (nbit : integer := 32);
     port (r1, r2: in std_logic_vector(nbit-1 downto 0);
           conf : in std_logic_vector(1 downto 0);               --00 sll, 01 srl, 10 sra
+          en_shifter : in std_logic;
           shifted_out : out std_logic_vector(nbit-1 downto 0));
 end component;
 
 signal r1_s, r2_s:  std_logic_vector(31 downto 0);
 signal conf_s :  std_logic_vector(1 downto 0);               --00 sll, 01 srl, 10 sra
+signal en_shifter_s : std_logic;
 signal shifted_out_s :  std_logic_vector(31 downto 0);
     
 begin
 
-shifter_dut : t2_shifter generic map (nbit=> 32) port map (r1_s, r2_s, conf_s, shifted_out_s);
+shifter_dut : t2_shifter generic map (nbit=> 32) port map (r1_s, r2_s, conf_s, en_shifter_s, shifted_out_s);
 
 VectProc:process
 begin
    --sll
-   r1_s <= x"0000000f"; r2_s <= x"00000000"; conf_s <= "01";
+   r1_s <= x"0000000f"; r2_s <= x"00000000"; conf_s <= "01"; en_shifter_s <= '0';
+   wait for 10 ns;
+
+   assert (shifted_out_s = x"00000000" error "enable");
+
+   en_shifter_ <= '1';
    wait for 10 ns;
    
    for I in 0 to 31 loop
