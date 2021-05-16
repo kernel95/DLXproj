6a2c4c93896180a103ee94fd1b84bb04e7426422
Kristina Rogacheva <33569375+tinaisaunicorn@users.noreply.github.com>
2021-05-16 19:15:56 +0200

    booth mul update testbench
    still ugly tho

--- boothmul/ugly testbench/tb_booth.vhd
+++ boothmul/ugly testbench/tb_booth.vhd
@@ -1,8 +1,8 @@
 ----------------------------------------------------------------------------------
--- Create Date: 11.03.2021
+-- Create Date: 16.05.2021
 -- Module Name: testbench 
 -- Project Name: DLX
--- Version: 1.0
+-- Version: 2.0 -- fixed the ugly testbench
 -- Additional Comments: testbench for the multiplier (Booth algorithm)
 ----------------------------------------------------------------------------------
 
@@ -19,23 +19,48 @@ architecture test of tb_booth is
 
   signal A_mp_i : std_logic_vector(numBit-1 downto 0) := (others => '0');
   signal B_mp_i : std_logic_vector(numBit-1 downto 0) := (others => '0');
-
+  signal en_mp_i: std_logic;
   signal Y_mp_i : std_logic_vector(N-1 downto 0) := (others => '0');
 
-  component boothmul 
-  generic(NBIT : integer := 32;
-          N: integer := 16;
-          M: integer := 16);
-  port(a: in std_logic_vector(N-1 downto 0);
-       b: in std_logic_vector(M-1 downto 0);
-       mul: out std_logic_vector(NBIT-1 downto 0));
+  component mul_wrapper 
+	generic( NBIT: integer := 32;
+              N: integer := 16;
+              M: integer := 16);
+  port   (a: IN  std_logic_vector(N-1 downto 0);
+          b: IN  std_logic_vector(M-1 downto 0);
+         en: IN  std_logic;
+        mul: OUT std_logic_vector(NBIT-1 downto 0));
   end component;
 
   begin
 
-    dut: boothmul port map (a=>A_mp_i, b=>B_mp_i, mul=>Y_mp_i);
-
-    A_mp_i <= "0000011111111111", "0000000000000010" after 30 ns, "1111111111000000" after 60 ns;
-    B_mp_i <= "1111111111101000", "0000000000011000" after 30 ns, "1111111111111100" after 60 ns;
+    dut: mul_wrapper port map (a=>A_mp_i, b=>B_mp_i, en=>en_mp_i, mul=>Y_mp_i);
     
+    process
+    begin
+    
+    en_mp_i <= '1', '0' after 5 ns, '1' after 10 ns;
+    
+    A_mp_i <= x"0004";
+    B_mp_i <= x"0004";
+    wait for 11 ns;
+    assert (Y_mp_i = x"00000010") report "error positive";
+    
+    A_mp_i <= x"fff6";
+    B_mp_i <= x"0004";
+    wait for 10 ns;
+    assert (Y_mp_i = x"ffffffd8") report "error 1 neg";
+    
+    A_mp_i <= x"0004";
+    B_mp_i <= x"fff6";
+    wait for 10 ns;
+    assert (Y_mp_i = x"ffffffd8") report "error 2 neg";
+    
+    A_mp_i <= x"fff6";
+    B_mp_i <= x"fff6";
+    wait for 10 ns;
+    assert (Y_mp_i = x"00000064") report "error both neg";
+    
+    wait;
+    end process;
 end test;
