5bbaedeef4395f17aded4271dfa77d3e655e6e55
kernel95 <s269182@studenti.polito.it>
2021-08-29 17:44:03 +0200

    comparator in the decode has been changed. Check before changing the wrapper

--- DataPath/Decode/comparator_addr.vhd
+++ DataPath/Decode/comparator_addr.vhd
@@ -1,5 +1,5 @@
 ----------------------------------------------------------------------------------
--- Create Date: 16.08.2021
+-- Create Date: 29.08.2021
 -- Module Name: comparator_addr
 -- Project Name: DLX
 -- Version: 1.0
@@ -7,17 +7,43 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.all;
 
 
 entity comparator_addr is
   Port (y1, y2: IN std_logic_vector(31 downto 0);
+        comp_control: IN std_logic_vector(1 downto 0); --control signal to decide which jump has to be performed
         EqualD: OUT std_logic);
 end comparator_addr;
 
-architecture df of comparator_addr is
-signal tmp1,tmp2 : std_logic;
+architecture Behavioral of comparator_addr is
+
 begin
+    process(comp_control,y1,y2)
+    begin
     
-    EqualD <= '1' when y1 = y2 else '0';
+    if(comp_control = "00") then --case you want compare both input
+      if (unsigned(y1) = unsigned(y2)) then
+        EqualD <= '1';
+      else
+        EqualD <= '0';
+      end if;
+    elsif (comp_control = "01") then --case you want to do just J
+      EqualD <= '1';
+    elsif (comp_control = "10") then --case you want to do BEQZ
+      if(unsigned(y1) = 0) then
+        EqualD <= '1';
+      else
+        EqualD <= '0';
+      end if; 
+    elsif (comp_control = "11") then --case you want to do BNEZ
+      if(unsigned(y1) = 0) then
+        EqualD <= '0';
+      else
+        EqualD <= '1';
+      end if;
     
-end df;
+    end if;
+    end process;
+    
+end Behavioral;
