d882781d99f8b261ac3603330d7e94f6f3e1dbdf
Giuseppe Carrubba <s269182@studenti.polito.it>
2021-10-09 18:50:48 +0200

    wrapper with pipeline for CU added with relative TB, not working right now

---
+++ ControlUnit/CU_wrapper/CU_wrapper.vhd
@@ -0,0 +1,194 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.myTypes.all;
+
+entity CU_wrapper is
+  Port (--INPUT
+        clock, reset: in std_logic;
+        OPCODE: in std_logic_vector(OP_CODE_SIZE - 1 downto 0);
+        FUNC  : in std_logic_vector(FUNC_SIZE - 1 downto 0);
+        EqualD: in std_logic;
+        FlushE: in std_logic;
+        --OUTPUT
+        
+        --DECODE
+        PC_SrcD :   out std_logic;
+        Select_ext: out std_logic;
+        IsJal:      out std_logic;
+        RD1:        out std_logic; -- ENALBE READ PORT 1 OF RF -- TO DO IN DATAPATH
+        RD2:        out std_logic; -- ENABLE READ PORT 2 OF RF -- TO DO IN DATAPATH 
+        Comp_control: out std_logic_vector(1 downto 0); -- control which branch to compute
+            
+        --EXECUTE
+        ALUcontrolE: OUT std_logic_vector(5 downto 0); -- DECODER SIGNAL ALU
+        RegDestE:    OUT std_logic; --select first mux of execute stage
+        ALUSrcE:     OUT std_logic; --select second mux of execute stage
+           
+        --MEMORY
+        MemWriteM: out std_logic;
+            
+        --WB
+        RegWriteW: out std_logic;
+        MemToRegW: out std_logic;
+            
+        --TO HAZARD UNIT            FOR THE WRAPPER
+        BranchD_H: OUT std_logic; 
+        MemToRegE_H: OUT std_logic;
+        RegWriteE_H: OUT std_logic;
+        MemToRegM_H: OUT std_logic;
+        RegWriteM_H: OUT std_logic;
+        RegWriteW_H: OUT std_logic;
+        -- FOR RF      
+        CALL, RET: OUT std_logic;
+        FILL, SPILL: IN std_logic);
+            
+end CU_wrapper;
+
+architecture Behavioral of CU_wrapper is
+
+component CU 
+       port (-- INPUTS
+              Rst : in std_logic;   -- Active Low              
+              OPCODE : in  std_logic_vector(OP_CODE_SIZE - 1 downto 0);
+              FUNC   : in  std_logic_vector(FUNC_SIZE - 1 downto 0);
+              -- FETCH
+              BranchD    : out std_logic;            -- Sel mux on Program Counter and Clear Pipe Regs between F and D
+              --DECODE
+              Select_ext: out std_logic; --signal for sign extend 
+              IsJal:  OUT std_logic; -- control signal to set muxes to address R31
+              RD1:    out std_logic; -- ENALBE READ PORT 1 OF RF -- TO DO IN DATAPATH
+              RD2:    out std_logic; -- ENABLE READ PORT 2 OF RF -- TO DO IN DATAPATH
+              RegWriteD:     out std_logic; --ENABLE WRITE PORT OF RF COMING FROM WRITEBACK ---- TO DO IN DATAPATH
+              Comp_control: OUT std_logic_vector(1 downto 0); --control signal for comparator that gives to CU the EqualD signal
+              --EXECUTE
+              RegDestD: OUT std_logic; --select first mux of execute stage
+              ALUSrcD: OUT std_logic; --select second mux of execute stage
+              ALUcontrolD: OUT std_logic_vector(5 downto 0); -- DECODER SIGNAL ALU
+              --MEMORY
+              MemWriteD: OUT std_logic; -- ENABLE WRITE PORT OF DATA MEMORY STAGE
+              --WB
+              MemToRegD: OUT std_logic; -- SEL OF MUX IN WRITEBACK STAGE
+              --FOR WINDOW REGISTER
+              CALL, RET: OUT std_logic;
+              FILL, SPILL: IN std_logic); 
+end component;
+
+--PIPELINE REGS
+--DECODE
+signal RegWriteD_reg,   RegWriteD_regnext:   std_logic;
+signal RegDestD_reg,    RegDestD_regnext:    std_logic;
+signal ALUSrcD_reg,     ALUSrcD_regnext:     std_logic;
+signal ALUcontrolD_reg, ALUcontrolD_regnext: std_logic_vector(5 downto 0);
+signal MemWriteD_reg,   MemWriteD_regnext:   std_logic; 
+signal MemToRegD_reg,   MemToRegD_regnext:   std_logic; 
+--EXECUTE
+signal RegWriteE_reg,   RegWriteE_regnext:   std_logic;
+signal MemWriteE_reg,   MemWriteE_regnext:   std_logic;
+signal MemToRegE_reg,   MemToRegE_regnext:   std_logic;
+--MEMORY
+signal RegWriteM_reg,   RegWriteM_regnext:   std_logic;
+signal MemToRegM_reg,   MemToRegM_regnext:   std_logic;
+--WB
+
+--SIGNALS CU
+signal BranchD_CU:        std_logic;
+signal IsJal_CU:          std_logic;
+signal select_ext_CU:     std_logic;
+signal RD1_CU, RD2_CU:    std_logic;
+signal Comp_control_CU:   std_logic_vector(1 downto 0);
+signal CALL_CU, RET_CU: std_logic;
+
+--signals for HazardUnit
+signal MemToRegE_CU : std_logic;
+signal RegWriteE_CU : std_logic; 
+signal MemToRegM_CU : std_logic;
+signal RegWriteM_CU : std_logic; 
+signal RegWriteW_CU : std_logic;
+
+begin
+
+Control : CU port map (RST => reset, 
+                       OPCODE => OPCODE, 
+                       FUNC => FUNC, 
+                       BranchD => BranchD_CU, 
+                       Select_ext => select_ext_CU, 
+                       isJal => IsJal_CU, 
+                       RD1 => RD1_CU, 
+                       RD2 => RD2_CU, 
+                       Comp_control => comp_control_CU, 
+                       RegWriteD => RegWriteD_regnext,
+                       AluSrcD => ALUSrcD_regnext, 
+                       ALUControlD => ALUControlD_regnext, 
+                       MemWriteD => MemWriteD_regnext,
+                       MemToRegD => MemToRegD_regnext, 
+                       CALL => CALL_CU , RET => RET_CU, FILL => FILL, SPILL => SPILL);
+                             
+process(clock, reset)
+begin
+    if (reset = '1') then
+    
+        RegWriteD_reg   <= '0';
+        RegDestD_reg    <= '0'; 
+        ALUSrcD_reg     <= '0';
+        ALUcontrolD_reg <= (others => '0');
+        MemWriteD_reg   <= '0';
+        MemToRegD_reg   <= '0';
+        
+        RegWriteE_reg <= '0';
+        MemWriteE_reg <= '0';
+        MemToRegE_reg <= '0';
+        
+        RegWriteM_reg <= '0';
+        MemToRegM_reg <= '0';
+        
+    elsif(FlushE = '1') then
+        RegWriteD_reg   <= '0';
+        RegDestD_reg    <= '0'; 
+        ALUSrcD_reg     <= '0';
+        ALUcontrolD_reg <= (others => '0');
+        MemWriteD_reg   <= '0';
+        MemToRegD_reg   <= '0';
+            
+    elsif(rising_edge(clock)) then
+        RegWriteD_reg   <= RegWriteD_regnext;
+        RegDestD_reg    <= RegDestD_regnext;
+        ALUSrcD_reg     <= ALUSrcD_regnext;
+        ALUcontrolD_reg <= ALUcontrolD_regnext;
+        MemWriteD_reg   <= MemWriteD_regnext;
+        MemToRegD_reg   <= MemToRegD_regnext;
+        RegWriteE_reg   <= RegWriteE_regnext;
+        MemToRegE_reg   <= MemToRegE_regnext;
+        MemWriteE_reg   <= MemWriteE_regnext;
+        RegWriteM_reg   <= RegWriteM_regnext;
+        MemToRegM_reg   <= MemToRegM_regnext;
+    end if;
+    
+end process;
+    --DECODE SIGNALS ASSIGNMENT
+    PC_SrcD      <= BranchD_CU AND EqualD;
+    select_ext   <= select_ext_CU;
+    IsJal        <= IsJal_CU;
+    RD1          <= RD1_CU;
+    RD2          <= RD2_CU;
+    Comp_control <= Comp_control_CU;
+    CALL <= CALL_CU;
+    RET <= RET_CU;
+    --EXECUTE SIGNALS ASSIGNMENT
+    AluControlE  <= ALUcontrolD_regnext;
+    AluSrcE      <= ALUSrcD_regnext;
+    RegDestE     <= RegDestD_regnext;
+    --MEMORY SIGNALS ASSINMENT
+    MemWriteM    <= MemWriteE_regnext;
+    RegWriteW    <= RegWriteM_regnext;
+    MemToRegW    <= MemToRegM_regnext;
+    --WB SIGNALS ASSIGNMENT
+    
+    --HAZARD UNIT
+    BranchD_H    <= BranchD_cu; 
+    MemToRegE_H  <= MemToRegE_regnext;
+    RegWriteE_H  <= RegWriteE_regnext;
+    MemToRegM_H  <= MemWriteE_regnext;
+    RegWriteM_H  <= RegWriteM_regnext;
+    RegWriteW_H  <= RegWriteM_regnext;
+    
+end Behavioral;
---
+++ ControlUnit/CU_wrapper/TB/TB_CU_wrapper.vhd
@@ -0,0 +1,133 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.all;
+use work.myTypes.all;
+
+entity TB_CU_Wrapper is
+--  Port ( );
+end TB_CU_Wrapper;
+
+architecture Behavioral of TB_CU_Wrapper is
+
+component CU_wrapper 
+  Port (--INPUT
+        clock, reset: in std_logic;
+        OPCODE: in std_logic_vector(OP_CODE_SIZE - 1 downto 0);
+        FUNC  : in std_logic_vector(FUNC_SIZE - 1 downto 0);
+        EqualD: in std_logic;
+        FlushE: in std_logic;
+        --OUTPUT
+        
+        --DECODE
+        PC_SrcD :   out std_logic;
+        Select_ext: out std_logic;
+        IsJal:      out std_logic;
+        RD1:        out std_logic; -- ENALBE READ PORT 1 OF RF -- TO DO IN DATAPATH
+        RD2:        out std_logic; -- ENABLE READ PORT 2 OF RF -- TO DO IN DATAPATH 
+        Comp_control: out std_logic_vector(1 downto 0); -- control which branch to compute
+            
+        --EXECUTE
+        ALUcontrolE: OUT std_logic_vector(5 downto 0); -- DECODER SIGNAL ALU
+        RegDestE:    OUT std_logic; --select first mux of execute stage
+        ALUSrcE:     OUT std_logic; --select second mux of execute stage
+           
+        --MEMORY
+        MemWriteM: out std_logic;
+            
+        --WB
+        RegWriteW: out std_logic;
+        MemToRegW: out std_logic;
+            
+        --TO HAZARD UNIT            FOR THE WRAPPER
+        BranchD_H:   OUT std_logic; 
+        MemToRegE_H: OUT std_logic;
+        RegWriteE_H: OUT std_logic;
+        MemToRegM_H: OUT std_logic;
+        RegWriteM_H: OUT std_logic;
+        RegWriteW_H: OUT std_logic;
+        -- FOR RF      
+        CALL, RET:   OUT std_logic;
+        FILL, SPILL: IN std_logic);
+end component;
+
+--TB SIGNALS
+signal clock_s, reset_s: std_logic;
+signal OPCODE_s: std_logic_vector(OP_CODE_SIZE - 1 downto 0);
+signal FUNC_s  : std_logic_vector(FUNC_SIZE - 1 downto 0);
+signal EqualD_s: std_logic;
+signal FlushE_s: std_logic;     
+--DECODE
+signal PC_SrcD_s :     std_logic;
+signal Select_ext_s:   std_logic;
+signal IsJal_s:        std_logic;
+signal RD1_s:          std_logic; -- ENALBE READ PORT 1 OF RF -- TO DO IN DATAPATH
+signal RD2_s:          std_logic; -- ENABLE READ PORT 2 OF RF -- TO DO IN DATAPATH 
+signal Comp_control_s: std_logic_vector(1 downto 0); -- control which branch to compute
+--EXECUTE
+signal ALUcontrolE_s:  std_logic_vector(5 downto 0); -- DECODER SIGNAL ALU
+signal RegDestE_s:     std_logic; --select first mux of execute stage
+signal ALUSrcE_s:      std_logic; --select second mux of execute stage
+ --MEMORY
+signal MemWriteM_s:    std_logic;
+--WB
+signal RegWriteW_s:    std_logic;
+signal MemToRegW_s:    std_logic;
+--TO HAZARD UNIT 
+signal BranchD_H_s:    std_logic; 
+signal MemToRegE_H_s:  std_logic;
+signal RegWriteE_H_s:  std_logic;
+signal MemToRegM_H_s:  std_logic;
+signal RegWriteM_H_s:  std_logic;
+signal RegWriteW_H_s:  std_logic;
+--FOR RF          
+signal CALL_s, RET_s:   std_logic;
+signal FILL_s, SPILL_s: std_logic;
+
+signal OPCODE_addr: std_logic_vector(OP_CODE_SIZE - 1 downto 0);
+signal FUNC_addr  : std_logic_vector(FUNC_SIZE - 1 downto 0);
+
+begin
+
+DUT : CU_wrapper port map (clock_s, reset_s, OPCODE_s, FUNC_s, EqualD_s, FlushE_s,
+                           PC_SrcD_s, Select_ext_s, IsJal_s, RD1_s, RD2_s, Comp_control_s,
+                           ALUcontrolE_s, RegDestE_s, ALUSrcE_s, MemWriteM_s, RegWriteW_s,
+                           MemToRegW_s, BranchD_H_s, MemToRegE_H_s, RegWriteE_H_s, MemToRegM_H_s,
+                           RegWriteM_H_s, RegWriteW_H_s, CALL_s, RET_s, FILL_s, SPILL_s);
+                           
+ClkProc:process(clock_s)
+begin
+    clock_s <= not(clock_s) after 5 ns;
+end process ClkProc;
+
+VectProc:process
+begin
+
+reset_s <= '1';
+OPCODE_addr <= (others => '0');
+FUNC_addr   <= (others => '0');
+EqualD_s <= '0';
+FlushE_s <= '0';
+
+wait for 5 ns;
+
+for i in 0 to 89 loop
+    
+    FUNC_addr <= std_logic_vector(unsigned(FUNC_addr) + 1);
+    FUNC_s <= FUNC_addr;
+    
+    if( i > 45 ) then
+       FUNC_addr   <= (others => '0');
+       FUNC_s <= FUNC_addr;
+       OPCODE_addr <= std_logic_vector(unsigned(OPCODE_addr) + 1);
+       OPCODE_s <= OPCODE_addr;
+    end if;
+    wait for 10 ns;
+    end loop;
+    
+    wait for 5 ns;
+    wait for 5 ns;
+    wait for 5 ns;
+wait;
+end process;
+
+end Behavioral;
